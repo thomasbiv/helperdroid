cc6dbfd556b75637cf2e910c4b0e41bf55f70660
Thomas Bivins <bivinsthomas@gmail.com>
2022-07-03 19:44:56 -0400

    tokenization

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import discord
 from discord.ext.commands import bot
 from discord.ext import commands
 from discord.ext.commands.help import HelpCommand
-TOKEN = "place token here"
 
+bot = commands.Bot(command_prefix='^')
+bot.remove_command("help")
+
+@bot.event
